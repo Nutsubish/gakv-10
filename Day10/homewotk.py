85078be8cf7fe4febf77a3aca267e48e4618d193
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-22 14:50:41 +0400

    Add files via upload

---
+++ Day10/homewotk.py
@@ -0,0 +1,46 @@
+#0 dan 100is chatvilt kentebi da luwebi da tan miuwevot kentia da luwia 
+#tavisi asaki vkitxot mamasis asaki da tu mamamisi 2x metia say bingo da if not mashin mouse
+#sqesis kitxva da age tu age >65 mashin:pensia gekutvins ogonc tu kacia, tu qalia da >60wlisaa: pensia gekutvins
+
+
+# num=1
+# i=1+1
+
+# for num in range(99):
+#     print(num+i)
+#     if num + i:2
+#     print("luwia chems qvevit")
+#     for num in range(1):
+#      if not num + i:2
+#      print("chems zevit kentia")
+
+
+
+  
+# yo_age = int(input("Enter the yo age: "))
+
+
+# dad_age = int(input("Enter the dad's age: "))
+
+
+# if dad_age / 2 == yo_age:
+#     print("Bingo!")
+# else:
+#     print("mouse")
+
+# print("choose 1 if you male choose 2 if you female on sqesi")
+
+
+# sqesi=1
+# sqesi=2
+
+
+
+# sqesi= input("sqesi:")
+# age= int(input("yo age"))
+# if sqesi is 1 :
+#   if age >65:
+#      print("wadi pensiaze")
+
+#      if not age >65 :
+#         print("go to back to wok u black slave")
